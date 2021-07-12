d8d0528153c93992a1736bdc798008ef8ed25b26
Prithvi1573 <73021105+Prithvi1573@users.noreply.github.com>
2021-07-12 08:07:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+music1="";
+music2="";
+function preload()
+{
+    music1=loadSound("music.mp3");
+    music2=loadSound("music2.mp3");
+}
+function setup()
+{
+        canvas= createCanvas(600,500);
+        canvas.center()
+
+        video=createCapture(VIDEO);
+        video.hide();
+}
+function modelLoaded()
+{
+console.log('LOODEDEMODEL')
+}
